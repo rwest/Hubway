357108d73fb66dcfbccd77e51d4e064e194d521d
Martin West <martinwest@Martins-Mac-mini.local>
2012-04-08 00:59:24 +0200

    made changes to block the matrix into smaller chunks

--- hubway.py
+++ hubway.py
@@ -14,6 +14,7 @@ import urllib
 import json
 import os
 import numpy
+import time
 
 class Station():
 	def __init__(self):
@@ -24,6 +25,29 @@ class Station():
 		return self.lat+','+self.long
 	pass
 
+
+def distancematrix(startplaces,endplaces):
+    geo_args = {
+    	'origins': startplaces,
+    	'destinations': endplaces,
+    	'mode': 'bicycling',
+    	'sensor': "false",
+    	}
+    BASE_URL = 'http://maps.googleapis.com/maps/api/distancematrix/json'
+    url = BASE_URL + '?' + urllib.urlencode(geo_args)
+    result = json.load(urllib2.urlopen(url))
+    assert(result['status']!='OVER_QUERY_LIMIT')
+
+    num_stations = len(stations)
+    times = numpy.zeros((len(startplaces), len(endplaces)), dtype=numpy.int32) # times in seconds
+    distances = numpy.zeros((len(startplaces), len(endplaces)), dtype=numpy.int32) # distances in m
+    for i,row in enumerate(result['rows']):
+    	for j,element in enumerate(row['elements']):
+    		times[i][j] = element['duration']['value']
+    		distances[i][j] = element['distance']['value']
+    return times,distances
+        
+        
 stations_file = urllib2.urlopen('http://thehubway.com/data/stations/bikeStations.xml')
 
 tree = ElementTree()
@@ -36,29 +60,42 @@ for s in tree.iter('station'):
 	station.long = s.find('long').text
 	station.name = s.find('name').text
 	stations.append(station)
-	
-stations = stations[:10] # just deal with first 10 for now, as there's a cap on calls to google API
 
 
-places = '|'.join([str(s) for s in stations])
-geo_args = {
-	'origins': places,
-	'destinations': places,
-	'mode': 'bicycling',
-	'sensor': "false",
-	}
-BASE_URL = 'http://maps.googleapis.com/maps/api/distancematrix/json'
-url = BASE_URL + '?' + urllib.urlencode(geo_args)
-result = json.load(urllib2.urlopen(url))
-assert(result['status']!='OVER_QUERY_LIMIT')
+number_of_hackers=2
+this_hacker=0 #hacker number 0 of 2
+assert this_hacker < number_of_hackers
+number_each=int(numpy.ceil(len(stations)/float(number_of_hackers)))
 
-num_stations = len(stations)
-times = numpy.zeros((num_stations, num_stations), dtype=numpy.int32) # times in seconds
-distances = numpy.zeros((num_stations, num_stations), dtype=numpy.int32) # distances in m
-for i,row in enumerate(result['rows']):
-	for j,element in enumerate(row['elements']):
-		times[i][j] = element['duration']['value']
-		distances[i][j] = element['distance']['value']
+startstations=stations[this_hacker*number_each:(this_hacker+1)*number_each]
+endstations=stations
+number_of_rowblocks=int(numpy.ceil(number_each/10.0))
+number_of_colblocks=int(numpy.ceil(len(stations)/10.0))
 
-times
+for idx_row in xrange(number_of_rowblocks):
+    for idx_col in xrange(number_of_colblocks):
+        startplaces_ids = '|'.join([str(s.id).rjust(2) for s in stations[this_hacker*number_each:(this_hacker+1)*number_each][idx_row*10:(idx_row+1)*10] ])
+        endplaces_ids = '|'.join([str(s.id).rjust(2) for s in stations[(idx_col*10):(idx_col+1)*10]])
+        
+        startplaces = '|'.join([str(s) for s in stations[this_hacker*number_each:(this_hacker+1)*number_each][idx_row*10:(idx_row+1)*10] ])
+        endplaces = '|'.join([str(s) for s in stations[(idx_col*10):(idx_col+1)*10]])
+        print 'start ' + startplaces_ids +',  end ' + endplaces_ids
+         
+        
+        distfilename = 'dist_matrix_'+str(this_hacker*number_of_rowblocks+idx_row)+'_'+str(idx_col)+'.numpy'
+        timefilename = 'time_matrix_'+str(this_hacker*number_of_rowblocks+idx_row)+'_'+str(idx_col)+'.numpy'
+        if os.path.exists(distfilename) and os.path.exists(timefilename):
+            print 'skipping ' + distfilename + ' and ' + timefilename
+                
+        else:
+            times,distances = distancematrix(startplaces,endplaces)
+            with open(distfilename,'w') as f:
+                    distances.tofile(f)
+            with open(timefilename,'w') as f:
+                    distances.tofile(f)
+            print 'writing ' + distfilename + ' and ' + timefilename      
+            time.sleep(numpy.random.rand()*2+12)
+            
+
+
 
